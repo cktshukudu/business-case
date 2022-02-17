97608084b6357440772ca9221fa97c1fe2e3e04b
christian <cktshukudu@gmail.com>
2022-02-18 00:39:43 +0200

    styled the tamplate page

--- solutions/urls.py
+++ solutions/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
+
 urlpatterns = [
     path('btamplate/', include('btamplate.urls')),
     path('admin/', admin.site.urls),
 ]
+
