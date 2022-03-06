622926ca82c82a1376b0305aaa26ad026f47288c
christian <cktshukudu@gmail.com>
2022-03-06 22:07:56 +0200

    migrated to mysql database

--- solutions/settings.py
+++ solutions/settings.py
@@ -86,11 +86,14 @@ WSGI_APPLICATION = 'solutions.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'solutions',
+        'USER': 'root',
+        'PASSWORD': 'Number@123',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
