54f3183b57e8c65787ac28da895ca4d7366c69c5
christian <cktshukudu@gmail.com>
2022-04-11 11:30:13 +0200

    solved base layout

--- solutions/settings.py
+++ solutions/settings.py
@@ -89,7 +89,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'solutions',
         'USER': 'root',
-        'PASSWORD': '800531Mosima.',
+        'PASSWORD': 'Number@123',
         'HOST': '127.0.0.1',
         'PORT': '3306',
     }
